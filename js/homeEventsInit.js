1fed5a50c4ccd066325d23fbcce468111962ac87
rpascal <ryanpascal99@yahoo.com>
2015-04-03 13:07:16 -0400

    removing key

--- js/homeEventsInit.js
+++ js/homeEventsInit.js
@@ -1,7 +1,7 @@
 ﻿$(function () {
     $('.eventList').googleCalendarList({
-        'key': 'AIzaSyCIwgW_o0spPcW92j7zM2tBo20cSlaHe5k',
-        'calendarIds': ["89t4rmh7d5abnqbdppbspq7k1g@group.calendar.google.com","jo8fgk2hrrqsgr0b2ujckpn5ac@group.calendar.google.com"],
+        'key': 'API KEY',
+        'calendarIds': ["calendar ids"],
         'fullEventsWithDateSelect': false,
         'shortenedEvents': true,
         'holderName': '.eventList',
